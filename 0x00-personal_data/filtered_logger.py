2bc5f40eb07ee10bcb6fb774bbddabcb94b6c7b0
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-04 18:57:58 +0000

    Task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,8 +3,13 @@
 Filtered logger file
 """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     regex_p = re.compile(r'({})=[^{}]*'.format('|'.join(fields), separator))
     return regex_p.sub(r'\1={}'.format(redaction), message)
