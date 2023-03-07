18b8f7c82b77189d42e7b6f51339fc5dfb65c8d4
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-07 15:06:29 +0000

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """ Module of Basic_Auth class
 """
 import re
+import base64
+import binascii
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +22,18 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+    ) -> str:
+        """Returns decoded value  of Base64-encoded string authorization header
+        """
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(
+                base64_authorization_header, validate=True)
+            return decoded.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
