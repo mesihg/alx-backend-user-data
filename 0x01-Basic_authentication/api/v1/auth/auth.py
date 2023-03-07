91f1b917ff69c0f312452fcfeab2f650eca5b7e0
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-07 12:40:42 +0000

    defining authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if path requires authentication
         """
-        return False
+        if path is not None or excluded_paths is not None:
+            for excluded_path in excluded_paths:
+                if path == excluded_path[:-1]:
+                    return False
+                elif path[len(excluded_path)-1] == '/':
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Get the authorization header field from the request
