bd536b25d94367e93da7358ce673324b4d704008
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-16 19:39:13 +0000

    code fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 """
 import bcrypt
 from uuid import uuid4
+from typing import Union
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
@@ -60,14 +61,17 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id: str) -> str:
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Return user from session or None
         """
+        user = None
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
-            return user.email
         except NoResultFound:
             return None
+        return user
 
     def destroy_session(self, user_id: int) -> None:
         """Destroy user session
