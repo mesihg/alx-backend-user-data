acfd50ef880b2dbad85bc19ead45f91b528057d1
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-16 19:08:21 +0000

    Return user from session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,3 +59,12 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """Return user from session or None
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user.email
+        except NoResultFound:
+            return None
