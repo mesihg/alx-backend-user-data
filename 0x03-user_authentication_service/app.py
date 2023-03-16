46e21e4994f64c6b05573f2667e3feb990751af4
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-16 19:27:16 +0000

    Destroy user session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -44,7 +44,7 @@ def login() -> str:
 
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout() -> str:
-    """Destroy user session
+    """Delete user session
     """
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
