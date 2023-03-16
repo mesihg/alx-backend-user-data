8787b991007d537b7e5e8a3192eb9985bf888f25
Mesih Grmay <mesihgrmay@yahoo.com>
2023-03-16 19:10:51 +0000

    Destroy user session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,3 +68,12 @@ class Auth:
             return user.email
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy user session
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+        except NoResultFound:
+            return None
